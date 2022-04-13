a85efe0624070d84fca6284d204be180b0cea5fe
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2022-04-13 12:10:13 +0530

    added vishal component

---
+++ src/component/vishal.js
@@ -0,0 +1,12 @@
+const { Component } = require("react");
+
+class Vishal extends Component{
+    render(){
+        return(
+            <div>
+                <h1>my first react line</h1>
+            </div>
+        )
+    }
+}
+export default Vishal;
